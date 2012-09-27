0cc3a8556e9776aa8df2e3de63525006edda563e
Joseph Bashe <bashedev@users.noreply.github.com>
2012-09-27 18:58:52 +0200

    gave option to change mode so that error handling either logs or displays formatting

--- db.php
+++ db.php
@@ -1,74 +1,118 @@
 <?php
 
-class db {
+class db
+{
+    private $dbhost = '';
+    private $dbname = '';
+    private $dbpw = '';
+    private $dbuser = '';
 
-  private $dbhost = '';
-  private $dbname = '';
-  private $dbpw = '';
-  private $dbuser = '';
+    public function __construct($dbname, $dbuser, $dbpass, $dbhost = 'localhost', $mode = 'dev')
+    {
+        $this->dbhost = $dbhost;
+        $this->dbname = $dbname;
+        $this->dbpass = $dbpass;
+        $this->dbuser = $dbuser;
 
-  public function __construct($dbname, $dbuser, $dbpw, $dbhost = 'localhost') {
-    $this->dbhost = $dbhost;
-    $this->dbname = $dbname;
-    $this->dbpw = $dbpw;
-    $this->dbuser = $dbuser;
-  }
-
-  public function conn() {
-    try {
-      $dbh = new PDO("mysql:host={$this->dbhost};dbname={$this->dbname}", $this->dbuser, $this->dbpw);
-      $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // for debugging
-      return $dbh;
-    } catch (PDOException $exc) {
-      error_log($exc->getMessage());
+        $this->mode = $mode;
     }
-    return false;
-  }
 
-  /**
-   *
-   * safeExecute tries to execute the PDOStatement. If a false response is returned
-   * from the database, the database error message is logged. If a PDOException is
-   * caught, the PDOException error message is logged as well.
-   *
-   * @param PDOStatement $stmt Fully prepared PDOStatement to be executed.
-   * @return <bool> True on success, false on query error or exception.
-   */
-  public function safeExecute(PDOStatement &$stmt) {
-    try {
-      if ($stmt->execute()) {
-        return true;
-      } else {
-        error_log(implode(' - ', $stmt->errorInfo()));
-      }
-    } catch (PDOException $exc) {
-      error_log($exc->getMessage());
+    public function conn()
+    {
+        try
+        {
+            $dbh = new PDO("mysql:host={$this->dbhost};dbname={$this->dbname}", $this->dbuser, $this->dbpw);
+            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // for debugging
+            return $dbh;
+        }
+        catch (PDOException $exc)
+        {
+            $this->handleException($exc);
+        }
+        return false;
     }
-    return false;
-  }
 
-  /**
-   * safeQuery tries to query the database with arbitrary sql code. Error checking
-   * similar to safeExecute is done. 
-   * 
-   * Note: this function opens and closes a db connection for every query.
-   * 
-   * @param string $sql
-   * @return mixed PDOStatement on success, boolean false on error.
-   */
-  public function safeQuery($sql) {
-    $conn = $this->conn();
-    try {
-      $stmt = $conn->query($sql);
-      if ($stmt) {
-        return $stmt;
-      } else {
-        error_log(implode(' - ', $stmt->errorInfo()));
-      }
-    } catch (PDOException $exc) {
-      error_log($exc->getMessage());
+    /**
+     *
+     * safeExecute tries to execute the PDOStatement. If a false response is returned
+     * from the database, the database error message is logged. If a PDOException is
+     * caught, the PDOException error message is logged as well.
+     *
+     * @param PDOStatement $stmt Fully prepared PDOStatement to be executed.
+     * @return <bool> True on success, false on query error or exception.
+     */
+    public function safeExecute(PDOStatement &$stmt)
+    {
+        try
+        {
+            if ($stmt->execute())
+            {
+                return true;
+            }
+            else
+            {
+                $this->handlePdoError($stmt);
+            }
+        }
+        catch (PDOException $exc)
+        {
+            $this->handleException($exc);
+        }
+        return false;
     }
-    $conn = null;
-  }
 
+    /**
+     * safeQuery tries to query the database with arbitrary sql code. Error checking
+     * similar to safeExecute is done. 
+     * 
+     * Note: this function opens and closes a db connection for every query.
+     * 
+     * @param string $sql
+     * @return mixed PDOStatement on success, boolean false on error.
+     */
+    public function safeQuery($sql)
+    {
+        $conn = $this->conn();
+        try
+        {
+            $stmt = $conn->query($sql);
+            if ($stmt)
+            {
+                return $stmt;
+            }
+            else
+            {
+                $this->handlePdoError($stmt);
+            }
+        }
+        catch (PDOException $exc)
+        {
+            $this->handleException($exc);
+        }
+        $conn = null;
+    }
+
+    private function handleException($exc)
+    {
+        if ($this->mode = 'dev')
+        {
+            echo $exc->getMessage();
+        }
+        else if ($this->mode = 'prod')
+        {
+            error_log($exc->getMessage());
+        }
+    }
+
+    private function handlePdoError($stmt)
+    {
+        if ($this->mode = 'dev')
+        {
+            print_r($stmt->errorInfo());
+        }
+        else if ($this->mode = 'prod')
+        {
+            error_log(implode(' - ', $stmt->errorInfo()));
+        }
+    }
 }
